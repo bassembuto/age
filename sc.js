616597865e31a9c9d3e9200aeb5a152645d3c2e9
bassembuto <132224648+bassembuto@users.noreply.github.com>
2023-09-14 19:42:38 +0300

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,11 @@
+function dmmod() {
+    document.querySelector('body').style.color = '#000';
+    document.querySelector('body').style.background = 'white';
+    document.querySelector('h1').style.color = 'blue';
+    document.querySelector('.h11').style.color = 'red';
+    document.querySelector('.h112').style.color = 'red';
+    document.querySelector('.dash').style.borderColor = 'blue';
+    document.querySelector('.darkmod').style.boxShadow = '1px 1px 5px #000';
+    document.querySelector('.darkmod2').style.boxShadow = '1px 1px 5px #000';
+
+}
